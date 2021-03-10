05fec6f5b7eb3d877ad71060042da4321948a17f
Joseph bayad <jobayad66@gmail.com>
2021-03-10 15:44:21 +0100

    password check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+
+const PasswordItem = ({ text, valid }) => {
+  return <li className={valid ? 'valid' : 'not-valid'}>{text}</li>;
+};
 function App() {
+  const [password, setPassword] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState({});
+
+  useEffect(() => {
+    // console.log('rege', /[A-Z]/.test(password));
+    setPasswordStrength({
+      minChar: password.length >= 8,
+      upperChar: /[A-Z]/.test(password),
+      number: /\d/.test(password),
+      specialChar: /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password),
+    });
+  }, [password]);
+
   return (
-    <div className='App'>
-      <h1>Hello React</h1>
+    <div className='container'>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <ul>
+          <PasswordItem
+            text='At least 8 characters'
+            valid={passwordStrength.minChar}
+          />
+          <PasswordItem
+            text='At least on number'
+            valid={passwordStrength.number}
+          />
+          <PasswordItem
+            text='At least on uppercase'
+            valid={passwordStrength.upperChar}
+          />
+          <PasswordItem
+            text='At least on special character'
+            valid={passwordStrength.specialChar}
+          />
+        </ul>
+      </div>
     </div>
   );
 }
